bc385e4f9f563a295e50657335b9ab0127da8463
duran89 <leral3@naver.com>
2024-01-23 03:54:28 +0900

    Add ToDo Data Model and Manager

---
+++ TodoListApp/Models/ToDo.swift
@@ -0,0 +1,8 @@
+//
+//  ToDo.swift
+//  TodoListApp
+//
+//  Created by 권정근 on 1/23/24.
+//
+
+import Foundation
---
+++ TodoListApp/Models/ToDoDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  ToDoDataManager.swift
+//  TodoListApp
+//
+//  Created by 권정근 on 1/23/24.
+//
+
+import Foundation
