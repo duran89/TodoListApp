daa743d0c879e81ef7973fbb7d43e5870f6e8677
duran89 <leral3@naver.com>
2024-01-23 04:12:25 +0900

    Add ToDoTableViewCell (compose cell)

---
+++ TodoListApp/Views/ToDoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ToDoTableViewCell.swift
+//  TodoListApp
+//
+//  Created by 권정근 on 1/23/24.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
